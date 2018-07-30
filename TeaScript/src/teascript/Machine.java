eeab120aafb26e2d9323fcba90a6c3b41673d282
AlnisS <alnis.smidchens@gmail.com>
2018-07-29 22:50:01 -0700

    some Machine doc

--- TeaScript/src/teascript/Machine.java
+++ TeaScript/src/teascript/Machine.java
@@ -10,21 +10,71 @@ import static teascript.Action.Type.*;
 import static teascript.TeaScript.*;
 import static teascript.Utils.loadStrings;
 
+/**
+ * Holds variables, Actions, Functions; handles setup and overall orchestration.
+ * Also has flags for the current line along with other misc data for setup.
+ * Runs an initial pass to define all global variables, labels, and functions.
+ * Handles swallowing script errors and makes sure stuff is mostly in order.
+ * 
+ * @author alnis
+ */
 public class Machine {
-
+    
+    /**
+     * Reference line written to for debugging and stuff when errors are thrown.
+     */
     int debugline;
+    /**
+     * All of the Actions in the script file in a single, absolute array.
+     */
     Action[] actions;
+    /**
+     * Dictionary of label names to absolute/global line numbers.
+     */
     IntDict labels;
+    /**
+     * List of dictionaries (one for each "scope") of names -> int var vals.
+     */
     ArrayList<FloatDict> floats;
+    /**
+     * List of dictionaries (one for each "scope") of names -> string var vals.
+     */
     ArrayList<StringDict> strings;
+    /**
+     * List of dictionaries (one for each "scope") of names -> boolean var vals.
+     */
     ArrayList<IntDict> booleans;
+    /**
+     * Used during setup for labels/functions to know where they are.
+     */
     int labeltemp;
+    /**
+     * Function names -> float functions.
+     */
     HashMap<String, Function> functions;
+    /**
+     * Function names -> string functions.
+     */
     HashMap<String, Function> sfunctions;
+    /**
+     * Function names -> boolean functions.
+     */
     HashMap<String, Function> bfunctions;
+    /**
+     * List (one map/"scope") of hashmaps for array name -> array of floats.
+     */
     ArrayList<HashMap<String, ArrayList<Float>>> farrs;
+    /**
+     * List (one map/"scope") of hashmaps for array name -> array of strings.
+     */
     ArrayList<HashMap<String, ArrayList<String>>> sarrs;
+    /**
+     * List (one map/"scope") of hashmaps for array name -> array of booleans.
+     */
     ArrayList<HashMap<String, ArrayList<Boolean>>> barrs;
+    /**
+     * Raw string contents of each line in the script file.
+     */
     String[] rawstrings;
 
     public Machine() {
